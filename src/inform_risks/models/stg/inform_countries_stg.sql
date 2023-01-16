5090d35f329322f2066ab64b019a0ca66d53a32d
amirbtb <76230761+amirbtb@users.noreply.github.com>
2023-01-16 18:08:10 +0100

    renommer colonne

--- src/inform_risks/models/stg/inform_countries_stg.sql
+++ src/inform_risks/models/stg/inform_countries_stg.sql
@@ -3,7 +3,7 @@ with source as (
 ),
 renamed as (
     select
-        cast("CategoryType" as {{ dbt.type_string() }}) as type_zone_geo,
+        cast("CategoryType" as {{ dbt.type_string() }}) as type_zone_geographique,
         cast("Iso3" as {{ dbt.type_string() }}) as code_iso3,
         cast("CountryName" as {{ dbt.type_string() }}) as nom_zone_geo,
         cast("IsoGroup" as {{ dbt.type_string() }}) as parent_zone_geo
